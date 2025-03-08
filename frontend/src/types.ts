41637aa9954765634a7a40e2ba2e5324ca0fedd4
celeste <ccsl5432@outlook.com>
2025-03-08 10:16:29 +0400

    wip: deleted the source folder, created a type file for country

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,5 +1,6 @@
-export interface Country {
+type Country = {
     id: string;
     name: string;
     opinion?: string;
-  }
+    status: string;
+    }
--- frontend/src/views/allCountries.vue
+++ frontend/src/views/allCountries.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
+
+import Country from './types.ts'
+import { defineComponent } from 'vue'
+import type { PropType } from 'vue'
+import type { Country } from './types'
+
+export default defineComponent({
+    props: {
+        country:{
+            type: Object as PropType<Country>,
+            required: true,
+        }
+    }
+
+
+    
+})
+ 
 </script>
 
 <template>
   <main>
+
   </main>
 </template>
